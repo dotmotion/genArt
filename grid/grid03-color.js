a0b80a3e532f58cb2751570d28a1b2d5fb5a0209
dotmotion <outon.x@gmail.com>
2018-12-17 12:55:37 -0600

    new grid version using a color palette

---
+++ grid/grid03-color.js
@@ -0,0 +1,64 @@
+const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+const palettes = require('nice-color-palettes')
+
+const settings = {
+  dimensions: [2048, 2048], // [ width, height ]
+  pixelsPerInch: 300 // DPI (resolution, default is 72)
+  //units: 'cm' //units can be modified to cemtimeters, inches, etc
+  //orientation: 'landscape'
+};
+
+const sketch = () => {
+  const palette = random.pick(palettes)
+  const createGrid = () => {
+    const points = [];
+    const count = 30;
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push({
+          color: random.pick(palette),
+          radius: Math.abs(random.gaussian() * 0.01),
+          position: [u, v]
+        });
+      }
+    }
+    return points;
+  };
+  //set a 'fixed' random numer
+  // random.setSeed(512);
+
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 200;
+
+  return ({ context, width, height }) => {
+    //background
+    context.fillStyle = "#16191b";
+    context.fillRect(0, 0, width, height);
+
+    points.forEach(data => {
+      const { 
+        position, 
+        radius,
+        color
+      } = data;
+
+      const [u, v] = position;
+
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, radius * width, 0, Math.PI * 2);
+      context.fillStyle = color;
+      context.lineWidth = 40;
+      context.fill();
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
