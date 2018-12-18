9c94d447ef4a6fddf5979dd1346474210bff6398
dotmotion <outon.x@gmail.com>
2018-12-18 17:57:20 -0600

    removed unused code and edited variables

--- grid/grid06-noise-symbols.js
+++ grid/grid06-noise-symbols.js
@@ -17,20 +17,20 @@ const settings = {
 
 const sketch = () => {
   // Generating a random color palette
-  const colorCount = random.rangeFloor(2, 6)
-  const palette = random.shuffle(random.pick(palettes))
-    .slice(0, colorCount)
+  const colorCount = random.rangeFloor(2, 7)
+  const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount)
+  console.log(palette)
 
   // Creating a point grid
   const createGrid = () => {
     const points = [];
-    const count = 50;
+    const count = 120;
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        const radius = Math.abs(random.noise2D(u, v)) * 0.2
+        const radius = Math.abs(random.noise2D(u, v)) * 0.08
         points.push({
           color: random.pick(palette),
           radius,
@@ -41,11 +41,12 @@ const sketch = () => {
     }
     return points;
   };
-  //set a 'fixed' random numer
-  // random.setSeed(512);
 
   const points = createGrid().filter(() => random.value() > 0.5);
-  const margin = 200;
+  const margin = 150;
+
+  //set a 'fixed' random numer
+  // random.setSeed(416294);
 
   return ({ context, width, height }) => {
     //background
@@ -62,21 +63,15 @@ const sketch = () => {
 
       const [u, v] = position;
 
-      const x = lerp(margin, width - margin, u);
-      const y = lerp(margin, height - margin, v);
-
-      // context.beginPath();
-      // context.arc(x, y, radius * width, 0, Math.PI * 2);
-      // context.fillStyle = color;
-      // context.lineWidth = 40;
-      // context.fill();
+      const x = lerp(margin, width - margin - 50, u);
+      const y = lerp(margin + 100, height - margin, v);
 
       context.save()
       context.fillStyle = color;
       context.font = `${radius * width}px "Helvetica"`
       context.translate(x, y)
       context.rotate(rotation)
-      context.fillText("⌁", 0, 0)
+      context.fillText("␥", 0, 0)
 
       context.restore()
     });
